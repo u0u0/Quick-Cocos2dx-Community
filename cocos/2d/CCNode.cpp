9f1741a998110200926294e97a9e5495d2819eac
zhaxin <zhaxin@gameeer.com>
2015-09-10 14:31:18 +0800

    remove touch event when a node is destroyed

--- cocos/2d/CCNode.cpp
+++ cocos/2d/CCNode.cpp
@@ -172,6 +172,7 @@ Node::~Node()
     {
         ScriptEngineManager::getInstance()->getScriptEngine()->removeScriptHandler(_updateScriptHandler);
     }
+    ScriptEngineManager::getInstance()->getScriptEngine()->removeTouchNodeEvent(this);
 #endif
 
     // User object has to be released before others, since userObject may have a weak reference of this node
--- external/lua/quick/lua_cocos2dx_quick_manual.cpp
+++ external/lua/quick/lua_cocos2dx_quick_manual.cpp
@@ -281,6 +281,19 @@ tolua_lerror:
 #endif
 }
 
+int removeLuaTouchNode(Node *node)
+{
+   auto mng = LuaNodeManager::getInstance();
+   auto lnode = mng->getLuaNodeByNode(node, false);
+   if (!lnode) {
+       return -1;
+   }
+   lnode->setLuaTouchEnabled(false);
+   lnode->detachNode();  //this LuaEventNode will be removed in TouchTargetNode
+   mng->removeLuaNode(lnode);
+   return 0;
+}
+
 static int tolua_Cocos2d_Node_removeTouchEvent(lua_State* tolua_S)
 {
 #if COCOS2D_DEBUG >= 1
@@ -297,16 +310,7 @@ static int tolua_Cocos2d_Node_removeTouchEvent(lua_State* tolua_S)
 #if COCOS2D_DEBUG >= 1
         if (!node) tolua_error(tolua_S,"invalid 'self' in function 'removeTouchEvent'", nullptr);
 #endif
-        {
-            auto mng = LuaNodeManager::getInstance();
-            auto lnode = mng->getLuaNodeByNode(node, false);
-            if (!lnode) {
-                return 0;
-            }
-            lnode->setLuaTouchEnabled(false);
-            lnode->detachNode();  //this LuaEventNode will be removed in TouchTargetNode
-            mng->removeLuaNode(lnode);
-        }
+        removeLuaTouchNode(node);
     }
     return 0;
 #if COCOS2D_DEBUG >= 1
