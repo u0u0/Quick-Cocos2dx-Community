0cec409f881a4b61acd80ab06f41f3a0bb3e789c
u0u0 <lkncastle@gmail.com>
2015-09-24 12:01:55 +0800

    disable cocos code ide support by default

--- quick/templates/shared/frameworks/runtime-src/Classes/CodeIDESupport.h
+++ quick/templates/shared/frameworks/runtime-src/Classes/CodeIDESupport.h
@@ -3,7 +3,7 @@
 
 // define 1 to open Cocos Code IDE support, 0 to disable
 #ifndef CC_CODE_IDE_DEBUG_SUPPORT
-#define CC_CODE_IDE_DEBUG_SUPPORT 1
+#define CC_CODE_IDE_DEBUG_SUPPORT 0
 #endif
 
 #endif /* __CODE_IDE_SUPPORT_H__ */
