f651a94ec9114fb5bdb32f54fa6ee8641fd185a0
u0u0 <lkncastle@gmail.com>
2015-11-23 22:23:16 +0800

    build freetype2 by ndk r10d. and update to freetype-2.5.5

--- external/freetype2/prebuilt/android/Android.mk
+++ external/freetype2/prebuilt/android/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_freetype2_static
 LOCAL_MODULE_FILENAME := freetype2
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libfreetype.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android $(LOCAL_PATH)/../../include/android/freetype2
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android/freetype2
 include $(PREBUILT_STATIC_LIBRARY)
