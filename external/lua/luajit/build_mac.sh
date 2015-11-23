3dec4f8ad808466f9bf2aff1857d8b8a3f4158db
u0u0 <lkncastle@gmail.com>
2015-11-23 11:57:21 +0800

    luajit android x86 build fix,  mac add i386.

--- external/lua/luajit/build_mac.sh
+++ external/lua/luajit/build_mac.sh
@@ -2,16 +2,26 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRCDIR=$DIR/src
 DESTDIR=$DIR/prebuilt/mac
-MACOSX_DEPLOYMENT_TARGET="10.6"
+#MACOSX_DEPLOYMENT_TARGET="10.6"
+LIPO="lipo"
+STRIP="strip"
 
 rm "$DESTDIR"/*.a
 
 cd $SRCDIR
 make clean
 make CC="gcc -m64 -arch x86_64" all
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-x86_64.a
 
-if [ -f $SRCDIR/src/libluajit.a ]; then
-    mv $SRCDIR/src/libluajit.a $DESTDIR/libluajit.a
-fi;
+make clean
+make CC="gcc -m32 -arch i386" all
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-i386.a
+
+make clean
+$LIPO -create "$DESTDIR"/libluajit-*.a -output "$DESTDIR"/libluajit.a
+$STRIP -S "$DESTDIR"/libluajit.a
+$LIPO -info "$DESTDIR"/libluajit.a
+
+rm "$DESTDIR"/libluajit-*.a
 
 make clean
