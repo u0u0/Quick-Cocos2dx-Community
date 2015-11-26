fa916083ca20cc904b8f281e1c9d7561d2440c82
u0u0 <lkncastle@gmail.com>
2015-11-26 12:43:49 +0800

    remove i386 from mac lib.

--- external/lua/luajit/build_mac.sh
+++ external/lua/luajit/build_mac.sh
@@ -10,18 +10,7 @@ rm "$DESTDIR"/*.a
 
 cd $SRCDIR
 make clean
-make CC="gcc -m64 -arch x86_64" all
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-x86_64.a
-
-make clean
-make CC="gcc -m32 -arch i386" all
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-i386.a
-
-make clean
-$LIPO -create "$DESTDIR"/libluajit-*.a -output "$DESTDIR"/libluajit.a
-$STRIP -S "$DESTDIR"/libluajit.a
-$LIPO -info "$DESTDIR"/libluajit.a
-
-rm "$DESTDIR"/libluajit-*.a
+make CC="gcc -arch x86_64" all
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit.a
 
 make clean
