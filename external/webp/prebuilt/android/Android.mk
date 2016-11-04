44c1322b056d2c16d98cefd8c9dff3bd4ddaad7a
u0u0 <hello@tyrantek.com>
2016-11-04 09:46:46 +0800

    lib webp update to 0.5.1

--- external/webp/prebuilt/android/Android.mk
+++ external/webp/prebuilt/android/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_webp_static
 LOCAL_MODULE_FILENAME := webp
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebpdecoder_static.a
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebp.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cpufeatures
