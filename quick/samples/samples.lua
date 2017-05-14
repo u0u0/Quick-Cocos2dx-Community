e2c34208c6d3a60bff685604d05284b4faa18c15
u0u0 <hello@tyrantek.com>
2017-05-14 21:51:14 +0800

    remove sample

--- quick/samples/samples.lua
+++ quick/samples/samples.lua
@@ -6,11 +6,6 @@ local data =
         description = "Api use case",
         path        = "samples/test",
     },
-    {
-        title       = "Online Update",
-        description = "一个Lua热更新示例",
-        path        = "samples/liveupdate",
-    },
 }
 
 return data
