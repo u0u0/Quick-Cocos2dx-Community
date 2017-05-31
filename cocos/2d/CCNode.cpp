66645dbcac63a43e819a5b190510ecf421bd113a
LiuKeNan <knliu@gamecdk.com>
2017-05-31 11:30:55 +0800

    bugfix：当在update中删除自己的时候，会导win32访问非法指针。调整调用顺序解决这个问题。

--- cocos/2d/CCNode.cpp
+++ cocos/2d/CCNode.cpp
@@ -1617,6 +1617,11 @@ void Node::pause()
 // override me
 void Node::update(float fDelta)
 {
+    if (_componentContainer && !_componentContainer->isEmpty())
+    {
+        _componentContainer->visit(fDelta);
+    }
+
 #if CC_ENABLE_SCRIPT_BINDING
     if (0 != _updateScriptHandler)
     {
@@ -1626,11 +1631,6 @@ void Node::update(float fDelta)
         ScriptEngineManager::getInstance()->getScriptEngine()->sendEvent(&event);
     }
 #endif
-    
-    if (_componentContainer && !_componentContainer->isEmpty())
-    {
-        _componentContainer->visit(fDelta);
-    }
 }
 
 // MARK: coordinates
