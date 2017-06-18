e438a7108d928d4b544a5b2e13423678a4633e3d
u0u0 <hello@tyrantek.com>
2017-06-18 23:07:20 +0800

    android build fix.

--- quick/lib/quick-src/extra/platform/android/CCHTTPRequestAndroid.cpp
+++ quick/lib/quick-src/extra/platform/android/CCHTTPRequestAndroid.cpp
@@ -365,7 +365,7 @@ void HTTPRequest::update(float dt)
     Director::getInstance()->getScheduler()->unscheduleAllForTarget(this);
     if (m_curlState != kCCHTTPRequestCURLStateIdle)
     {
-        Director::getInstance()->getScheduler()->schedule(schedule_selector(HTTPRequest::checkCURLState), this, 0, false);
+        Director::getInstance()->getScheduler()->schedule(CC_SCHEDULE_SELECTOR(HTTPRequest::checkCURLState), this, 0, false);
     }
 
     if (m_state == kCCHTTPRequestStateCompleted)
