b539bbd60525eddb1000b0af78a06352f6b83742
LiuKeNan <knliu@gamecdk.com>
2017-07-19 10:20:06 +0800

    rendertexture即时渲染，这样可适应begin end中间修改节点的属性来修改rendertexture的内容。

--- cocos/2d/CCRenderTexture.cpp
+++ cocos/2d/CCRenderTexture.cpp
@@ -584,7 +584,6 @@ void RenderTexture::onEnd()
     //
     director->loadMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_PROJECTION, _oldProjMatrix);
     director->loadMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW, _oldTransMatrix);
-
 }
 
 void RenderTexture::onClear()
@@ -726,10 +725,10 @@ void RenderTexture::end()
     Renderer *renderer = director->getRenderer();
     renderer->addCommand(&_endCommand);
     renderer->popGroup();
+    renderer->render();
     
     director->popMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_PROJECTION);
     director->popMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
-
 }
 
 NS_CC_END
