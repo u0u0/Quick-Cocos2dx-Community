50397221cd5a00df4a171393116172a233062421
LiuKeNan <knliu@gamecdk.com>
2018-01-08 14:23:31 +0800

    remove useless log on csb loading.

--- cocos/editor-support/cocostudio/ActionTimeline/CSLoader.cpp
+++ cocos/editor-support/cocostudio/ActionTimeline/CSLoader.cpp
@@ -239,7 +239,7 @@ Node* CSLoader::createNode(const std::string& filename)
     std::string path = filename;
     size_t pos = path.find_last_of('.');
     std::string suffix = path.substr(pos + 1, path.length());
-    CCLOG("suffix = %s", suffix.c_str());
+    //CCLOG("suffix = %s", suffix.c_str());
     
     CSLoader* load = CSLoader::getInstance();
     
@@ -260,7 +260,7 @@ ActionTimeline* CSLoader::createTimeline(const std::string &filename)
     std::string path = filename;
     size_t pos = path.find_last_of('.');
     std::string suffix = path.substr(pos + 1, path.length());
-    CCLOG("suffix = %s", suffix.c_str());
+    //CCLOG("suffix = %s", suffix.c_str());
     
     ActionTimelineCache* cache = ActionTimelineCache::getInstance();
     
@@ -802,12 +802,12 @@ Node* CSLoader::nodeWithFlatBuffers(const flatbuffers::NodeTree *nodetree)
     
     auto children = nodetree->children();
     int size = children->size();
-    CCLOG("size = %d", size);
+    //CCLOG("size = %d", size);
     for (int i = 0; i < size; ++i)
     {
         auto subNodeTree = children->Get(i);
         Node* child = nodeWithFlatBuffers(subNodeTree);
-        CCLOG("child = %p", child);
+        //CCLOG("child = %p", child);
         if (child)
         {
             PageView* pageView = dynamic_cast<PageView*>(node);
@@ -877,7 +877,7 @@ bool CSLoader::bindCallback(const std::string &callbackName,
         }
     }
     
-    CCLOG("callBackName %s cannot be found", callbackName.c_str());
+    //CCLOG("callBackName %s cannot be found", callbackName.c_str());
     
     return false;
     
@@ -1062,7 +1062,7 @@ Node* CSLoader::nodeWithFlatBuffersForSimulator(const flatbuffers::NodeTree *nod
     Node* node = nullptr;
     
     std::string classname = nodetree->classname()->c_str();
-    CCLOG("classname = %s", classname.c_str());
+    //CCLOG("classname = %s", classname.c_str());
     
     auto options = nodetree->options();
     
@@ -1071,7 +1071,7 @@ Node* CSLoader::nodeWithFlatBuffersForSimulator(const flatbuffers::NodeTree *nod
         auto reader = ProjectNodeReader::getInstance();
         auto projectNodeOptions = (ProjectNodeOptions*)options->data();
         std::string filePath = projectNodeOptions->fileName()->c_str();
-        CCLOG("filePath = %s", filePath.c_str());
+        //CCLOG("filePath = %s", filePath.c_str());
         
         if (filePath != "" && FileUtils::getInstance()->isFileExist(filePath))
         {
@@ -1112,12 +1112,12 @@ Node* CSLoader::nodeWithFlatBuffersForSimulator(const flatbuffers::NodeTree *nod
     
     auto children = nodetree->children();
     int size = children->size();
-    CCLOG("size = %d", size);
+    //CCLOG("size = %d", size);
     for (int i = 0; i < size; ++i)
     {
         auto subNodeTree = children->Get(i);
         Node* child = nodeWithFlatBuffersForSimulator(subNodeTree);
-        CCLOG("child = %p", child);
+        //CCLOG("child = %p", child);
         if (child)
         {
             PageView* pageView = dynamic_cast<PageView*>(node);
