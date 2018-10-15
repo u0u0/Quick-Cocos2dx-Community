b60e762fb882accd7054f0bfa2e5fe54d5be8571
LiuKeNan <knliu@gamecdk.com>
2018-10-15 09:34:35 +0800

    remove unused value.

--- quick/template/src/config.lua
+++ quick/template/src/config.lua
@@ -8,15 +8,6 @@ DEBUG_FPS = true
 -- dump memory info every 10 seconds
 DEBUG_MEM = false
 
--- load deprecated API
-LOAD_DEPRECATED_API = false
-
--- load shortcodes API
-LOAD_SHORTCODES_API = true
-
--- screen orientation
-CONFIG_SCREEN_ORIENTATION = "__SCREEN_ORIENTATION_L__"
-
 -- design resolution
 CONFIG_SCREEN_WIDTH  = __SCREEN_WIDTH__
 CONFIG_SCREEN_HEIGHT = __SCREEN_HEIGHT__
