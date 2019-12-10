e54d0a9877a677e6340b6563f4bf360242ed2921
u0u0 <hello@tyrantek.com>
2019-12-10 11:04:06 +0800

    setFrameSize不应该影响_designResolutionSize

--- cocos/platform/CCGLView.cpp
+++ cocos/platform/CCGLView.cpp
@@ -184,7 +184,7 @@ const Size& GLView::getFrameSize() const
 
 void GLView::setFrameSize(float width, float height)
 {
-    _designResolutionSize = _screenSize = Size(width, height);
+    _screenSize = Size(width, height);
 }
 
 Rect GLView::getVisibleRect() const
