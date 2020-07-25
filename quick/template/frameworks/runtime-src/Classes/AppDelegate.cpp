5afb5db06c2b2bde919722e5efeadde9cc4fa57d
u0u0 <hello@tyrantek.com>
2020-07-26 07:57:46 +0800

    pc检查64，已加载64的luajit bytecode

--- quick/template/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ quick/template/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -113,7 +113,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 #if 0
     LuaStack* stack = engine->getLuaStack();
     // use luajit bytecode package
-#if defined(__aarch64__) || defined(__arm64__)
+#if defined(__aarch64__) || defined(__arm64__) || defined(__x86_64__)
     stack->loadChunksFromZIP("res/game64.zip");
 #else
     stack->loadChunksFromZIP("res/game32.zip");
