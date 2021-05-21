790e8cdfd006943c98c8e90f4ef421212cc4b590
u0u0 <hello@tyrantek.com>
2021-05-21 20:16:09 +0800

    add mac arm64 build script

--- external/lua/luajit/build_mac.sh
+++ external/lua/luajit/build_mac.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIPO="xcrun -sdk iphoneos lipo"
+STRIP="xcrun -sdk iphoneos strip"
+
 SRCDIR=$DIR/src
 DESTDIR=$DIR/prebuilt/mac
+
+IXCODE=`xcode-select -print-path`
+ISDK=$IXCODE/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 
 rm "$DESTDIR"/*.a
 
 cd $SRCDIR
 export MACOSX_DEPLOYMENT_TARGET=10.9
+
 make clean
 make CC="clang -m64 -arch x86_64 -DLUAJIT_ENABLE_GC64" all
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit.a
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-x86_64.a
+
+make clean
+make HOST_CC="clang" TARGET_FLAGS="-arch arm64 -DLUAJIT_ENABLE_GC64 -isysroot $ISDK" TARGET=arm64 TARGET_SYS=Darwin
+mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-arm64.a
+
+make clean
+$LIPO -create "$DESTDIR"/libluajit-*.a -output "$DESTDIR"/libluajit.a
+$STRIP -S "$DESTDIR"/libluajit.a
+$LIPO -info "$DESTDIR"/libluajit.a
+
+rm "$DESTDIR"/libluajit-*.a
 
 make clean
